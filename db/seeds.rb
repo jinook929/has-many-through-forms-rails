4a8bb435df271b9382ac1a53762eb29e4b206e34
jinook929 <jinook929@gmail.com>
2021-03-16 22:54:31 -0400

    commit tree 14a250e303a87219c5a98be920e134f181622d1a

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create(name: "A")
+Category.create(name: "B")
+Category.create(name: "C")
+
+Post.create(title: "01", content: "hi hi hi")
+Post.create(title: "02", content: "hi hi hi")
+Post.create(title: "03", content: "hi hi hi")
+Post.create(title: "04", content: "hi hi hi")
+Post.create(title: "05", content: "hi hi hi")
+Post.create(title: "06", content: "hi hi hi")
+
+PostCategory.create(post_id: 1, category_id: 1)
+PostCategory.create(post_id: 1, category_id: 2)
+PostCategory.create(post_id: 1, category_id: 3)
+PostCategory.create(post_id: 2, category_id: 2)
+PostCategory.create(post_id: 3, category_id: 3)
+PostCategory.create(post_id: 4, category_id: 1)
+PostCategory.create(post_id: 5, category_id: 2)
+PostCategory.create(post_id: 6, category_id: 3)
